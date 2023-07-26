8c6bd38b47c7359c2105c5ca98099f11b9eec592
gunawanjason <bigcapital9999@gmail.com>
2023-07-26 11:52:19 +0800

    no message

--- api.js
+++ api.js
@@ -21,7 +21,7 @@ app.get('/:translation/:book/:chapter/:verse', (req, res) => {
   // Assuming the JSON files are named as 'translation.json', 'translation2.json', etc.
   const fileName = `${translation}.json`;
   // Get current directory
-  const jsonDirectory = path.join(process.cwd());
+  const jsonDirectory = path.join(process.cwd(), json);
 
   fs.readFile(jsonDirectory+"/"+fileName, 'utf8', (err, data) => {
     if (err) {
