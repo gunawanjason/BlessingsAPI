81eb9c1691caf438f8da3475243fe71675887e38
gunawanjason <bigcapital9999@gmail.com>
2023-07-26 13:49:50 +0800

    no message

--- api.js
+++ api.js
@@ -1,9 +1,10 @@
-import path from 'path';
-import { promises as fs } from 'fs';
+//import path from 'path';
+//import { promises as fs } from 'fs';
 
 const express = require('express');
 const bodyParser = require('body-parser');
-//const fs = require('fs');
+const fs = require('fs');
+const path = require('path')
 
 const app = express();
 const port = 3000; // Change this to the desired port number
@@ -52,7 +53,6 @@ function findVerse(data, bookName, chapter, verse) {
     return null;
   }
 }
-
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
